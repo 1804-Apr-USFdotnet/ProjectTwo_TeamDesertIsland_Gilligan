a021c9d0d57d17387c7bdec48f055688a4e1872c
Michael B <mjbradvica@users.noreply.github.com>
2018-05-14 20:30:46 -0400

    api model first pass
    api model first pass

---
+++ Gilligan.API/Gilligan.API.Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Gilligan.API.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int ZipCode { get; set; }
+        public string Email { get; set; }
+
+        public virtual ICollection<Rating> Ratings { get; set; }
+        public virtual ICollection<UserSong> UserSongs { get; set; }
+
+        public User()
+        {
+            Ratings = new HashSet<Rating>();
+        }
+    }
+}
