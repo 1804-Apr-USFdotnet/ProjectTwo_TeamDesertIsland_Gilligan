e015001c4a1d3b4dd4caf002ea80f2b4e686ee92
Michael B <mjbradvica@users.noreply.github.com>
2018-05-16 21:41:13 -0400

    reference fix
    reference fix

--- Gilligan.API/Gilligan.API.DomainServices/RatingService.cs
+++ Gilligan.API/Gilligan.API.DomainServices/RatingService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Gilligan.API.DomainContracts;
 using Gilligan.API.Models;
 using Gilligan.API.RepositoryContracts;
