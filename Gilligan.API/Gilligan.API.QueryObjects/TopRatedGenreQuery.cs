ef7d6591bf64d2a2763b25f744f43d85504ce715
MrLeslieAllenRice <mrleslieallenrice@icloud.com>
2018-05-20 16:20:06 -0400

    featureAddedQueryObjectImplementations and Unit Tests
    Added Query Object Implementations / Unit Tests

--- Gilligan.API/Gilligan.API.QueryObjects/TopRatedGenreQuery.cs
+++ Gilligan.API/Gilligan.API.QueryObjects/TopRatedGenreQuery.cs
@@ -3,11 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gilligan.API.Models;
 
 namespace Gilligan.API.QueryObjects
 {
-    class TopRatedGenreQuery
+    public class TopRatedGenreQuery
     {
+            private readonly IEnumerable<Genre> _list;
+            private readonly int _takeAmount;
+            private readonly DateTime _oneMonthAgo;
+            private readonly DateTime _oneWeekAgo;
 
+            public TopRatedGenreQuery(IEnumerable<Genre> list, int takeAmount)
+            {
+                _list = list;
+                _takeAmount = takeAmount;
+                _oneMonthAgo = DateTime.Today - TimeSpan.FromDays(31);
+                _oneWeekAgo = DateTime.Today - TimeSpan.FromDays(7);
+            }
+
+            public List<Genre> Monthly()
+            {
+                return _list.Where(w => w.Artists.Any(x => x.Songs.Any(y => y.Ratings.Any(z => z.RatedOn >= _oneMonthAgo))))
+                            .OrderByDescending(x => x.Artists.Select(y => y.Songs.Select(z => z.AverageRating)))
+                            .Take(_takeAmount)
+                            .ToList();
+
+
+
+            var genres = _list.Average(x => x.Artists.Select(y => y.Songs.Select(z => z.AverageRating)));
+            }
+
+            public List<Genre> Weekly()
+            {
+                return _list.Where(w => w.Artists.Any(x => x.Songs.Any(y => y.Ratings.Any(z => z.RatedOn >= _oneWeekAgo))))
+                            .OrderByDescending(x => x.Artists.Join(y => y.Songs()))
+                            .Take(_takeAmount)
+                            .ToList();
+            }
+
+            public List<Genre> Daily()
+            {
+                return _list.Where(w => w.Artists.Any(x => x.Songs.Any(y => y.Ratings.Any(z => z.RatedOn == DateTime.Today))))
+                            .OrderByDescending(x => x.Artists.Any(y => y.Songs.Average(z => z.AverageRating)))
+                            .Take(_takeAmount)
+                            .ToList();
+            }
+
+            public List<Genre> AllTime()
+            {
+                return _list.OrderByDescending(x => x.Artists.Any(y => y.Songs.Average(z => z.AverageRating)))
+                            .Take(_takeAmount)
+                            .ToList();
+            }
     }
 }
