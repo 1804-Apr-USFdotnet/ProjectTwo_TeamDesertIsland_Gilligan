21d4478dca838b1fe639fd77fd5a654bdd368f18
Michael B <mjbradvica@users.noreply.github.com>
2018-05-24 11:07:07 -0400

    web api cookie auth
    web api cookie auth

--- Gilligan.API/Gilligan.API.Rest/App_Start/WebApiConfig.cs
+++ Gilligan.API/Gilligan.API.Rest/App_Start/WebApiConfig.cs
@@ -7,9 +7,14 @@ namespace Gilligan.API.Rest
 {
     public static class WebApiConfig
     {
+        public static string AuthenticationType = "AuthTestCookie";
+        public static string CookieName = "AuthTestCookie";
+
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors();
+            config.Filters.Add(new AuthorizeAttribute());
 
             // Web API routes
             config.MapHttpAttributeRoutes();
--- Gilligan.API/Gilligan.API.Rest/Controllers/UserController.cs
+++ Gilligan.API/Gilligan.API.Rest/Controllers/UserController.cs
@@ -1,6 +1,14 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
 using AutoMapper;
 using Gilligan.API.DomainContracts;
+using Gilligan.API.Models;
+using Gilligan.API.Repositories;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
 
 namespace Gilligan.API.Rest.Controllers
 {
@@ -13,6 +21,100 @@ namespace Gilligan.API.Rest.Controllers
         {
             _userService = userService;
             _mapper = mapper;
+        }
+
+        [HttpPost]
+        [Route("~/api/Account/Register")]
+        [AllowAnonymous]
+        public IHttpActionResult Register(User account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            // actually register
+            var userStore = new UserStore<IdentityUser>(new GilliganContext());
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var user = new IdentityUser(account.UserName);
+
+            if (userManager.Users.Any(u => u.UserName == account.UserName))
+            {
+                return BadRequest();
+            }
+
+            userManager.Create(user, account.Password);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("~/api/Account/RegisterAdmin")]
+        [AllowAnonymous]
+        public IHttpActionResult RegisterAdmin(User account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            // actually register
+            var userStore = new UserStore<IdentityUser>(new GilliganContext());
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var user = new IdentityUser(account.UserName);
+
+            if (userManager.Users.Any(u => u.UserName == account.UserName))
+            {
+                return BadRequest();
+            }
+
+            userManager.Create(user, account.Password);
+
+            // the only difference from Register action
+            userManager.AddClaim(user.Id, new Claim(ClaimTypes.Role, "admin"));
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("~/api/Account/Login")]
+        [AllowAnonymous]
+        public IHttpActionResult LogIn(User account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            // actually login
+            var userStore = new UserStore<IdentityUser>(new GilliganContext());
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var user = userManager.Users.FirstOrDefault(u => u.UserName == account.UserName);
+
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            if (!userManager.CheckPassword(user, account.Password))
+            {
+                return Unauthorized();
+            }
+
+            var authManager = Request.GetOwinContext().Authentication;
+            var claimsIdentity = userManager.CreateIdentity(user, WebApiConfig.AuthenticationType);
+
+            authManager.SignIn(new AuthenticationProperties { IsPersistent = true }, claimsIdentity);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("~/api/Account/Logout")]
+        public IHttpActionResult Logout()
+        {
+            Request.GetOwinContext().Authentication.SignOut(WebApiConfig.AuthenticationType);
+            return Ok();
         }
     }
 }
